9475964b42ccd761420c1be62ee59339b1aac52f
sb-elliot-7s <s.babichevv91p@gmail.com>
2024-02-06 15:24:35 +0300

    add common handlers

---
+++ routers/common_router.py
@@ -0,0 +1,29 @@
+from aiogram import Router, types, F
+from aiogram.filters import CommandStart, Command
+
+from keyboards.start_keyboards import start_keyboard
+
+common_router = Router(name='common_router')
+
+
+@common_router.message(CommandStart())
+async def start_handler(message: types.Message):
+    user = message.from_user
+    """
+    save user in db
+    """
+    await message.answer_photo(
+        photo='https://etimg.etb2bimg.com/photo/84847436.cms',
+        caption=f' Данный бот поможет вам быстро выбрать блюдо, оплатить и получить ваш заказ',
+        reply_markup=start_keyboard()
+    )
+
+
+@common_router.message(Command('help'))
+async def help_handler(message: types.Message):
+    await message.answer(text='help message')
+
+
+@common_router.message(F.text == 'Показать контакты')
+async def contacts_handler(message: types.Message):
+    await message.reply(text='Телефон: +89007773355\n\nEmail: sobaka@gmail.com\n\nhttps://delivery-food.com')
