0329b79bda4395d03df2d74f73cd2b507a85e828
sb-elliot-7s <s.babichevv91p@gmail.com>
2024-02-06 14:02:10 +0300

    add start keyboard

---
+++ keyboards/start_keyboards.py
@@ -0,0 +1,9 @@
+from .keyboard_builders import reply_keyboard_builder
+
+
+def start_keyboard():
+    return reply_keyboard_builder(
+        texts=['Показать контакты', 'Поиск'],
+        sizes=[2, 1],
+        resize_keyboard=True
+    )
