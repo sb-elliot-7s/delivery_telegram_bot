a00f24b9a28aa04792e90d92a02066aa963add95
sb-elliot-7s <s.babichevv91p@gmail.com>
2024-02-11 16:45:05 +0300

    add 2 functions: add first state and update state

--- states/admin.py
+++ states/admin.py
@@ -1,3 +1,5 @@
+from aiogram import types
+from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 
 
@@ -10,3 +12,17 @@ class AddDishState(StatesGroup):
 
 class DeleteDishState(StatesGroup):
     dish_id = State()
+
+
+async def update_state(state: FSMContext, message: types.Message, message_state_data: str, msg: str, next_state: State,
+                       key):
+    await state.update_data({key: message_state_data})
+    await message.answer(text=msg)
+    await state.set_state(next_state)
+
+
+async def set_first_state(callback_message: types.CallbackQuery, state: FSMContext, first_state: State,
+                          message_text: str):
+    await callback_message.answer()
+    await callback_message.message.answer(text=message_text)
+    await state.set_state(first_state)
