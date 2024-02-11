5b0c339158f98c3908b6704b71625dab77326f27
sb-elliot-7s <s.babichevv91p@gmail.com>
2024-02-11 17:14:32 +0300

    add DishCallbackData

--- keyboards/admin_keyboards.py
+++ keyboards/admin_keyboards.py
@@ -1,9 +1,19 @@
+from aiogram.filters.callback_data import CallbackData
+
 from .keyboard_builders import inline_keyboard_builder
+
+
+class DishCallbackData(CallbackData, prefix='dish'):
+    action: str
 
 
 def admin_keyboard():
     return inline_keyboard_builder(
         texts=['Добавить блюдо', 'Удалить блюдо', 'Статистика'],
-        callback_datas=['add_good', 'delete_good', 'stats'],
-        sizes=[3]
+        callback_datas=[
+            DishCallbackData(action='add').pack(),
+            DishCallbackData(action='delete').pack(),
+            'stats'
+        ],
+        sizes=[2, 1]
     )
