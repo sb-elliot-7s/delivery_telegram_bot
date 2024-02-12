d4fda6358d7eb294ef9609975273be708a094f98
sb-elliot-7s <s.babichevv91p@gmail.com>
2024-02-13 00:21:37 +0300

    write get_dishes and get_dish method

--- db/dish.py
+++ db/dish.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from bson import ObjectId
 from motor.motor_asyncio import AsyncIOMotorCollection
 
+from schemas.dish import DishSchema, DishResultSchema
+
 
 @dataclass
 class Dish:
@@ -13,3 +15,12 @@ class Dish:
 
     async def delete_dish(self, dish_id: str):
         return (await self.collection.delete_one(filter={'_id': ObjectId(dish_id)})).deleted_count
+
+    async def get_dishes(self, skip: int = 0, limit: int = 0):
+        return [DishSchema(**dish) async for dish in self.collection.find().limit(limit=limit).skip(skip=skip)]
+
+    async def get_dish(self, dish_id: str):
+        document = await self.collection.find_one(filter={'_id': ObjectId(dish_id)})
+        if document is None:
+            return DishResultSchema(message='Dish not found', dish=None, status_code=404)
+        return DishResultSchema(message='OK', dish=DishSchema(**document), status_code=200)
