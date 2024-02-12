1688a43b21d2557ca70a3de9faa17ff607221d93
sb-elliot-7s <s.babichevv91p@gmail.com>
2024-02-13 00:18:07 +0300

    add dish result schema

--- schemas/dish.py
+++ schemas/dish.py
@@ -1,8 +1,40 @@
-from pydantic import BaseModel
+from typing import Annotated, Any
+
+from bson import ObjectId
+from pydantic import BaseModel, Field, ConfigDict
+from pydantic import PlainSerializer, AfterValidator, WithJsonSchema
 
 
-class DishSchema(BaseModel):
+class CreateDishSchema(BaseModel):
     name: str
     description: str
     photo: str
     price: str
+
+
+def validate_object_id(v: Any) -> ObjectId:
+    if isinstance(v, ObjectId):
+        return v
+    if ObjectId.is_valid(v):
+        return ObjectId(v)
+    raise ValueError("Invalid ObjectId")
+
+
+PyObjectId = Annotated[
+    str | ObjectId,
+    AfterValidator(validate_object_id),
+    PlainSerializer(lambda x: str(x), return_type=str),
+    WithJsonSchema({"type": "string"}, mode="serialization"),
+]
+
+
+class DishSchema(CreateDishSchema):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    id: PyObjectId = Field(alias='_id')
+
+
+class DishResultSchema(BaseModel):
+    dish: DishSchema | None
+    message: str
+    status_code: int
