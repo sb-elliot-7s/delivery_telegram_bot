3de838f16fdcdcf5c3d8e6625419bdb276275f7f
sb-elliot-7s <s.babichevv91p@gmail.com>
2024-02-13 17:04:37 +0300

    create py_object_id

---
+++ schemas/obj_id.py
@@ -0,0 +1,20 @@
+from typing import Any, Annotated
+
+from bson import ObjectId
+from pydantic import PlainSerializer, WithJsonSchema, AfterValidator
+
+
+def validate_object_id(v: Any) -> ObjectId:
+    if isinstance(v, ObjectId):
+        return v
+    if ObjectId.is_valid(v):
+        return ObjectId(v)
+    raise ValueError("Invalid ObjectId")
+
+
+PyObjectId = Annotated[
+    str | ObjectId,
+    AfterValidator(validate_object_id),
+    PlainSerializer(lambda x: str(x), return_type=str),
+    WithJsonSchema({"type": "string"}, mode="serialization"),
+]
