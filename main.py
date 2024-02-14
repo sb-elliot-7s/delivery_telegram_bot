3626123d7f346f81a9aa91723ae554a846b059f9
sb-elliot-7s <s.babichevv91p@gmail.com>
2024-02-14 12:34:48 +0300

    add payment router

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ from configs import settings
 from routers.common_router import common_router
 from routers.admin_router import admin_router
 from routers.dish_router import dish_router
+from routers.payment_router import payment_router
 
 
 async def main():
     logging.basicConfig(level=logging.INFO)
     dp = Dispatcher()
-    dp.include_routers(common_router, admin_router, dish_router)
+    dp.include_routers(common_router, admin_router, dish_router, payment_router)
     bot = Bot(token=settings.bot_token, parse_mode=ParseMode.MARKDOWN_V2)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
