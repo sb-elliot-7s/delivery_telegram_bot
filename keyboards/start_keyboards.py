b739d38908544a1b96cfb9bb4930e694ae0b16a4
sb-elliot-7s <s.babichevv91p@gmail.com>
2024-02-14 12:15:37 +0300

    add menu button

--- keyboards/start_keyboards.py
+++ keyboards/start_keyboards.py
@@ -3,7 +3,7 @@ from .keyboard_builders import reply_keyboard_builder
 
 def start_keyboard():
     return reply_keyboard_builder(
-        texts=['Показать контакты', 'Поиск'],
+        texts=['Показать контакты', 'Поиск', 'Меню'],
         sizes=[2, 1],
         resize_keyboard=True
     )
