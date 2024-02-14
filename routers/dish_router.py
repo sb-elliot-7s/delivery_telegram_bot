4dd175b9a27deab41996aece15d9ff5afe44b605
sb-elliot-7s <s.babichevv91p@gmail.com>
2024-02-14 12:14:48 +0300

    add empty dish answer

--- routers/dish_router.py
+++ routers/dish_router.py
@@ -1,5 +1,4 @@
-from aiogram import Router, types
-from aiogram.filters import Command
+from aiogram import Router, types, F
 
 from db.dish import Dish
 from db.base import dish_collection
@@ -9,9 +8,11 @@ from utils.text_utils import get_caption_dish
 dish_router = Router(name='dish_router')
 
 
-@dish_router.message(Command('dishes'))
+@dish_router.message(F.text == 'Меню')
 async def show_dishes(message: types.Message):
     dishes = await Dish(collection=dish_collection).get_dishes()
+    if not dishes:
+        return await message.answer(text='Список блюд пуст')
     for dish in dishes:
         caption: str = get_caption_dish(name=dish.name, description=dish.description, price=dish.price)
         await message.answer_photo(photo=dish.photo, caption=caption, reply_markup=pay_keyboard(dish_data=dish))
