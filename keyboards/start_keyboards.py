4f0dda0e9e92782f1e8eca590e41f6731103e1dc
sb-elliot-7s <s.babichevv91p@gmail.com>
2024-02-16 10:46:36 +0300

    change start keyboard

--- keyboards/start_keyboards.py
+++ keyboards/start_keyboards.py
@@ -2,8 +2,4 @@ from .keyboard_builders import reply_keyboard_builder
 
 
 def start_keyboard():
-    return reply_keyboard_builder(
-        texts=['Показать контакты', 'Поиск', 'Меню'],
-        sizes=[2, 1],
-        resize_keyboard=True
-    )
+    return reply_keyboard_builder(texts=['Показать контакты', 'Меню'], sizes=[2], resize_keyboard=True)
