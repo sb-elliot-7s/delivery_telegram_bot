a3b4e31ac20c9de93863cd43a42b12711247cd57
sb-elliot-7s <s.babichevv91p@gmail.com>
2024-02-17 15:59:38 +0300

    add conversation router

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ from routers.common_router import common_router
 from routers.admin_router import admin_router
 from routers.dish_router import dish_router
 from routers.payment_router import payment_router
+from routers.conversation_router import conversation_router
 
 
 async def main():
     logging.basicConfig(level=logging.INFO)
     dp = Dispatcher()
-    dp.include_routers(common_router, admin_router, dish_router, payment_router)
+    dp.include_routers(common_router, admin_router, dish_router, payment_router, conversation_router)
     bot = Bot(token=settings.bot_token, parse_mode=ParseMode.MARKDOWN_V2)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
