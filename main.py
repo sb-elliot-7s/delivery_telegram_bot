c604a5de93cf72cada844a3bc47f657d84c8badb
sb-elliot-7s <s.babichevv91p@gmail.com>
2024-02-28 09:17:18 +0300

    add retriever

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_pinecone import PineconeVectorStore
 
+from ai.store import RetrieverService
 from configs import settings
 from routers.common_router import common_router
 from routers.admin_router import admin_router
@@ -18,7 +21,10 @@ async def main():
     dp.include_routers(common_router, admin_router, dish_router, payment_router, conversation_router)
     bot = Bot(token=settings.bot_token, parse_mode=ParseMode.MARKDOWN_V2)
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    retriever = RetrieverService(vectorstore=PineconeVectorStore(
+        index_name=settings.pinecone_index_name, embedding=OllamaEmbeddings(model='gemma:2b')
+    )).get_retriever()
+    await dp.start_polling(bot, retriever=retriever)
 
 
 if __name__ == '__main__':
